6bb95c1a771c0d0018d08a20831bf6387e98b7a9
Wonkyu <lwk9589@gmail.com>
2025-07-13 04:59:21 +0900

    back/frontend init

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+// src/app.controller.ts
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Module({
-  imports: [], // 추후 다른 모듈들 (예: AuthModule 등)을 여기에 추가
-  controllers: [AppController], // 라우팅 처리
-  providers: [AppService], // 비즈니스 로직/서비스 제공
-})
-export class AppModule {}
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
