e11dedbd1da71824cd51c5383e5e5e2ea8b8ff42
Wonkyu <lwk9589@gmail.com>
2025-07-13 23:20:12 +0900

    의존성 주입 error fix

--- backend/src/stem-job/stem-job.module.ts
+++ backend/src/stem-job/stem-job.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StemJobService } from './stem-job.service';
 import { StemJobController } from './stem-job.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([])],
   controllers: [StemJobController],
   providers: [StemJobService],
+  exports: [StemJobService],
 })
 export class StemJobModule {}
