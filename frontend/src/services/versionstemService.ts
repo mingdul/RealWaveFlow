10eb14842bc0d2543a52e050947ee02d515ca1a8
NewOld21 <kshandy0221@gmail.com>
2025-07-14 23:53:02 +0900

    getMasterStemStreamingUrls 수정

---
+++ frontend/src/services/versionstemService.ts
@@ -0,0 +1,22 @@
+import apiClient from "../lib/api";
+
+class VersionStemService {
+    
+    async getLatestStemsPerCategoryByTrack(trackId: string, version: number) {
+        try {
+            const response = await apiClient.get(`/version-stem/track/${trackId}/version/${version}`);
+            if(!response.data.success){
+                throw new Error(response.data.message);
+            }
+            return response.data.data;
+        } catch (error) {
+            console.error('Failed to get latest stems per category by track:', error);
+            throw new Error('Failed to get latest stems per category by track');
+        }
+    }
+
+
+
+}
+
+export default new VersionStemService();
